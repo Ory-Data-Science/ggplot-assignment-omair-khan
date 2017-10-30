41431823a0a2b0ec227a6a916682163d3a221b5c
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-30 15:24:36 -0500

    its so blue

--- OK number 4.R
+++ OK number 4.R
@@ -2,3 +2,14 @@ require(tidyverse)
 dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan07.txt"),
                 sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999"))
 
+ggplot(data = dat, aes(x = F_mass)) +
+  labs(x ="Female Mass (g)") +
+  geom_histogram()
+
+
+ggplot(data = dat, aes(x = F_mass)) +
+  scale_x_log10() +
+  labs(x = "female mass (g)") + 
+  geom_histogram(fill = "blue")
+
+
