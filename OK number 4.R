66c1909b500dbbff00caf3559a84fd13d7df8195
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-30 15:20:53 -0500

    part 4 begins

---
+++ OK number 4.R
@@ -0,0 +1,4 @@
+require(tidyverse)
+dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan07.txt"),
+                sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999"))
+
